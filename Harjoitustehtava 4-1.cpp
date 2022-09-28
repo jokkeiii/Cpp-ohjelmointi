434c460ea4d45871aa2d7bd29434a0bc9f7334e1
Jokke <joakim.ijas@gmail.com>
2022-09-28 14:25:21 +0300

    Create Harjoitustehtava 4-1.cpp

---
+++ Harjoitustehtava 4-1.cpp
@@ -0,0 +1,31 @@
+// 4. Tunti - Viope 4 - 1
+// Yksinkertainen verolaskuri
+// Tampereella asuva maksaa kunnallisveroa bruttotuloistaan tänä vuonna 19.75%.
+//  Lisäksi valtiolle menee tuloveroa, jonka määrä riippuu tuloista seuraavan taulukon mukaisesti:
+/* 
+Verotettava ansiotulo, euroa	Vero alarajan kohdalla, euroa	Vero alarajan ylittavasta tulon osasta,  %
+16 900—25 300	                8	                            6,25
+25 300—41 200	                533	                            17,5
+41 200—71 400	                3 315,50	                    21,5
+71 400—	                        10 174	                        31,5 
+*/
+
+#include<iostream>              // cin-cout kirjaston linkkaaminen
+
+using namespace std;            // Nimiavaruus kaytossa
+
+int main() {
+    
+    float tulot, ;
+
+    cout << "Syota tulosi: ";
+    cin >> tulot;
+
+    if (tulot <= 16900)
+    {
+        /* code */
+    }
+    
+
+    return 0;
+}
