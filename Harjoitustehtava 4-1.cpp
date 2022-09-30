f88b29fbb3a1323ad123e84cc3755e5a597966cb
Jokke <joakim.ijas@gmail.com>
2022-09-30 10:23:14 +0300

    Luotiin 4-1 sisälle verolaskuri

--- Harjoitustehtava 4-1.cpp
+++ Harjoitustehtava 4-1.cpp
@@ -12,19 +12,42 @@ Verotettava ansiotulo, euroa	Vero alarajan kohdalla, euroa	Vero alarajan ylittav
 
 #include<iostream>              // cin-cout kirjaston linkkaaminen
 
-using namespace std;            // Nimiavaruus kaytossa
+using namespace std;            // Nimiavaruus standard kaytossa
 
 int main() {
     
-    float tulot, ;
+    float tulot, kunta_vero = 0.1975, yht_verot; // Tarvittavat muuttujat
 
-    cout << "Syota tulosi: ";
+    cout << "Syota tulosi: " << endl; // Kysytaan kayttajalta tulot
     cin >> tulot;
 
-    if (tulot <= 16900)
+    if (tulot >= 16900 && tulot < 25300) // Jos tulot ovat ensimmaisen vero-osan rajoissa
     {
-        /* code */
+        yht_verot = (tulot - 16900) * 0.0625 + 8 + (tulot * kunta_vero); // Lasketaan alarajan ylimenevasta osasta verot ja lisataan euromaaraan verot alarajan kohdalla
+        cout << "Sinun pitaa maksaa " << yht_verot << " euroa veroa."; 
     }
+    else if (tulot >= 25300 && tulot < 41200) // Jos tulot ovat toisen vero-osan rajoissa
+    {
+        yht_verot = (tulot - 16900) * 0.175 + 533 + (tulot * kunta_vero); 
+        cout << "Sinun pitaa maksaa " << yht_verot << " euroa veroa.";
+    }
+    else if (tulot >= 41200 && tulot < 71400) // Jos tulot ovat ensimmaisen vero-osan rajoissa
+    {
+        yht_verot = (tulot - 16900) * 0.215 + 3315.5 + (tulot * kunta_vero); 
+        cout << "Sinun pitaa maksaa " << yht_verot << " euroa veroa.";
+    }
+    else if (tulot >= 71400) // Jos tulot ovat ensimmaisen vero-osan rajoissa
+    {
+        yht_verot = (tulot - 71400) * 0.315 + 10174 + (tulot * kunta_vero); 
+        cout << "Sinun pitaa maksaa " << yht_verot << " euroa veroa.";
+    }
+    else // Jos tulot ovat alle ensimmaisen vero-osan rajan 
+    {
+       yht_verot = tulot * kunta_vero; // Jatetaan ansiotuloverot huomioimatta ja lasketaan ainoastaan kunnallisvero
+        cout << "Sinun pitaa maksaa " << yht_verot << " euroa veroa.";
+    }
+    
+    
     
 
     return 0;
