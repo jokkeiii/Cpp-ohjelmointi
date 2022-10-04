0da662128f6b876bac411ea72a320850f77fb7bf
Jokke <joakim.ijas@gmail.com>
2022-10-04 13:28:58 +0300

    Gitignore ja uusi tiedosto

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 5-3.cpp
@@ -0,0 +1,19 @@
+// 5. Tunti Viope 5 - 3
+// Silmukalla lukujen kysyminen ja lopussa summa
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+	int n = 0; 
+        do
+        {
+            cout << "anna yhteenlaskettavat luvut (kun haluat lopettaa laita luvuksi 0): ";
+            cin >> n;
+
+        } while (n > 0);
+	
+    return 0;
+}
