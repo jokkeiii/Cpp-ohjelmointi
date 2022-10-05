1298b736288eb45550929f97fc661f8f05a8e448
Jokke <joakim.ijas@gmail.com>
2022-10-05 18:58:08 +0300

    tiedoston luonti

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 5-2.cpp
@@ -0,0 +1,13 @@
+// 5. Tunti Viope 5 - 2
+// While –silmukan käyttöä. Tee tiliohjelma, joka pitää yllä saldoa ja jossa voit tallettaa ja nostaa rahaa.
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+
+	
+    return 0;
+}
