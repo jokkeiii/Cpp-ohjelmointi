5d36dc084ed92fcc130e7369f563590fc655ff06
Jokke <joakim.ijas@gmail.com>
2022-10-08 14:22:12 +0300

    commenting

--- Testipatkat/Silmukka-opettelua.cpp
+++ Testipatkat/Silmukka-opettelua.cpp
@@ -7,14 +7,20 @@ int main()
 	
 	float tyylipisteet = 0, tuomaripisteet;
 
-		for (int i = 1; i < 6; i++)
-		{
-			cout << "Anna " << i << ". Tuomarin pisteet: ";
-			cin >> tuomaripisteet; // Tassa kysytaan yksittaisen tuomarin pisteet
-			if (tuomaripisteet < 0 || tuomaripisteet > 20)
-				i--;
-			else
-				tyylipisteet += tuomaripisteet;
+	for (int i = 1; i < 6; i++)
+	{
+		// Tassa kysytaan yksittaisen tuomarin pisteet
+		cout << "Anna " << i << ". Tuomarin pisteet: ";
+		cin >> tuomaripisteet; 
+		
+		// Jos annetut pisteet ovat alle 0 tai yli 20 
+		// kysytaan kyseisen tuomarin pisteet uudelleen
+		if (tuomaripisteet < 0 || tuomaripisteet > 20)
+		i--;
+		
+		else {
+			tyylipisteet += tuomaripisteet;
 		}
+	}	
 	return 0;
 }
