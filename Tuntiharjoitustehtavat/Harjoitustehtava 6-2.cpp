b63584e056a0617d6d21e184a757d00433a90e0b
Jokke <joakim.ijas@gmail.com>
2022-10-11 11:34:11 +0300

    started 6-2

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 6-2.cpp
@@ -0,0 +1,26 @@
+// 6. Tunti Viope 6 - 2
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl; 
+
+int main() {
+    int sek;
+    float tulos;
+
+    cout << "anna aika sekunneissa: ";
+    cin >> sek;
+
+    cout << "Aika\tNopeus(km/h)";
+
+    for (int i = 0; i <= sek; i++)
+    {
+        tulos=
+    }
+    
+	
+    return 0;
+}
