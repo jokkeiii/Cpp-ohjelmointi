9e8cab1a4c030d1862d774e26cb0ff432b0fad23
Jokke <joakim.ijas@gmail.com>
2022-10-11 12:36:24 +0300

    updated malli

--- Tuntiharjoitustehtavat/Harjoitustehtava malli.cpp
+++ Tuntiharjoitustehtavat/Harjoitustehtava malli.cpp
@@ -1,14 +1,17 @@
-// x. Tunti Viope x - y
+// x. Tunti Viope x - b
 // Koodin toiminta selitettyna lyhyesti
 
 #include<iostream>
 
 using std::cout;
 using std::cin;
-using std::endl; 
+using std::endl;
+using std::setlocale;
 
 int main() {
+    setlocale(LC_ALL, "fi_FI");
+    
 
-	
+
     return 0;
 }
