3713a01743ff7be6bc1910005271b1f39d451d91
jokoni <joakim.ijas@tuni.fi>
2022-11-01 11:21:37 +0200

    made 7-1

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 7-1.cpp
@@ -0,0 +1,17 @@
+// 7. Tunti Viope 7 - 1
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setlocale;
+
+int main() {
+    setlocale(LC_ALL, "fi_FI");
+    
+
+
+    return 0;
+}
