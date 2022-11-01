9c0d31cc4de5e944a4d42ac8ce281dbc4eb47e21
jokoni <joakim.ijas@tuni.fi>
2022-11-01 12:51:07 +0200

    finished 7-3

--- Tuntiharjoitustehtavat/Harjoitustehtava 7-3.cpp
+++ Tuntiharjoitustehtavat/Harjoitustehtava 7-3.cpp
@@ -7,10 +7,27 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+float MatkanHinta(float, float, float);
+
 int main() {
     setlocale(LC_ALL, "fi_FI");
     
-    
+    float matka, kulutus, hinta;
+
+    cout << "Anna autolla ajettava matka kilometrina: ";
+    cin >> matka;
+
+    cout << "Anna auton polttoaineen kulutus sadalla kilometrilla: "; 
+    cin >> kulutus;
+
+    cout << "Anna polttoaineen hinta: "; 
+    cin >> hinta; 
+
+    cout << "Autolla ajettavan matkan polttoainekulut ovat: " << MatkanHinta(matka, kulutus, hinta) << " euroa." << endl;
 
     return 0;
+}
+
+float MatkanHinta(float km, float e_kulutus, float litra_hinta){
+    return km / 100 * e_kulutus * litra_hinta;
 }
