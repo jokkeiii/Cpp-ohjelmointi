894549c81dfdb2ce7ebe71c90d99c5b43fa555f4
jokoni <joakim.ijas@tuni.fi>
2022-11-01 11:46:45 +0200

    finished 7-1

--- Tuntiharjoitustehtavat/Harjoitustehtava 7-1.cpp
+++ Tuntiharjoitustehtavat/Harjoitustehtava 7-1.cpp
@@ -1,17 +1,26 @@
 // 7. Tunti Viope 7 - 1
-// Koodin toiminta selitettyna lyhyesti
+// Ohjelma palauttaa kayttajan syotteesta x * x
 
 #include<iostream>
 
 using std::cout;
 using std::cin;
 using std::endl;
-using std::setlocale;
+
+// Aliohjelma Nelio palauttaa x * x
+int Nelio(int x) { 
+    return x * x;
+}
 
 int main() {
-    setlocale(LC_ALL, "fi_FI");
+    int num, tulos; 
+
+    // Kysytaan kayttajalta syote lukuna
+    cout << "Anna luku, jonka nelio lasketaan: "; 
+    cin >> num;
+
+    // Tulostetaan suoraan kutsutun aliohjelman tulos 
+    cout << "Luvun Nelio on: " << Nelio(num) << endl;
     
-
-
     return 0;
 }
