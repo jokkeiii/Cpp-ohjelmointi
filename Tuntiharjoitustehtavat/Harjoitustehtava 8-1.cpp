2c4dd18df376a4f584324b8883b56aefed5ce33b
jokoni <joakim.ijas@tuni.fi>
2022-11-07 12:04:58 +0200

    created 8-1

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 8-1.cpp
@@ -0,0 +1,20 @@
+// 8. Tunti Viope 8 - 1
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int KertomaFunktio(int);
+
+int main() {
+    
+    
+    
+
+    return 0;
+}
+
+int KertomaFunktio(int x);
