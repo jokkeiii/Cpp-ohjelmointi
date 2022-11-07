a35290e0d9b66308c6a9048eb9810aaca1f527ff
jokoni <joakim.ijas@tuni.fi>
2022-11-07 12:15:36 +0200

    started 8-1

--- Tuntiharjoitustehtavat/Harjoitustehtava 8-1.cpp
+++ Tuntiharjoitustehtavat/Harjoitustehtava 8-1.cpp
@@ -5,16 +5,33 @@
 
 using std::cout;
 using std::cin;
-using std::endl;
+using std::endl; 
 
 int KertomaFunktio(int);
 
 int main() {
     
-    
-    
+    int luku, tulos;
 
+    cout << "Anna kokonaisluku välille 0 - 16" << endl; 
+    cin >> luku; 
+
+    // Jos tulos on 0 tulostetaan virhe
+    if (tulos == 0)
+    {
+        /* code */
+    }
+    
+    cout << "Luvun " << luku << " kertoma on " << tulos; 
+    
     return 0;
 }
 
-int KertomaFunktio(int x);
+int KertomaFunktio(int x){ 
+    // Jos x on negatiivinen tulostetaan 0
+    if (x < 0)
+    {
+        return 0;
+    }
+    
+}
