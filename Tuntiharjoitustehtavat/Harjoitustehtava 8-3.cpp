32b2868dabd63e482f1b93c2dd223f86c941e177
jokoni <joakim.ijas@tuni.fi>
2022-11-08 14:17:21 +0200

    started 8-3

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 8-3.cpp
@@ -0,0 +1,34 @@
+// 8. Tunti Viope 8 - 3
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+unsigned long Fibonacci(int x){
+
+
+}
+
+
+int main() {
+    setlocale(LC_ALL, "fi_FI");
+    
+    int syote; 
+    unsigned long fibonacci_num; 
+
+    cout << "Anna fibonaccin lukujen maara: ";
+    cin >> syote; 
+    cout << endl; 
+
+    cout << "Fibonacci Sarja : "; 
+
+    for (size_t i = 0; i < syote; i++){
+        /* code */
+    }
+    
+
+    return 0;
+}
