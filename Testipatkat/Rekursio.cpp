96831f0414ff05e08cbbad342269e57f28714ae4
jokoni <joakim.ijas@tuni.fi>
2022-11-08 13:47:29 +0200

    created rekursio

---
+++ Testipatkat/Rekursio.cpp
@@ -0,0 +1,16 @@
+// x. Tunti Viope x - b
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    setlocale(LC_ALL, "fi_FI");
+    
+    
+
+    return 0;
+}
