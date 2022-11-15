9daa0509248befaaf0c0ede34d44297a5461e72b
jokija <joakim.ijas@tuni.fi>
2022-11-15 12:04:14 +0200

    made 9-3

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 9-3.cpp
@@ -0,0 +1,36 @@
+// 9. Tunti Viope 9 - 3
+// Alkuluku vai ei
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+bool alkuluku(int n){
+
+    if (n < 2)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+int main() {
+    setlocale(LC_ALL, "fi_FI");
+
+    // Silmukka kutsuu aliohjelmaa luvuilla 0-1000
+    for (int i = 0; i < 1000; i++)
+    {
+        // Jos aliohjelman palautus true, tulostetaan kyseinen luku
+        if (alkuluku(i) == true)
+        {
+            cout << i << ", ";
+        }
+    
+    }
+    
+
+    return 0;
+}
