52b08bea68922bd6f69eea10753737a48307f947
jokija <joakim.ijas@tuni.fi>
2022-11-15 11:27:05 +0200

    finished 9-1

--- Tuntiharjoitustehtavat/Harjoitustehtava 9-1.cpp
+++ Tuntiharjoitustehtavat/Harjoitustehtava 9-1.cpp
@@ -1,5 +1,5 @@
 // 9. Tunti Viope 9 - 1
-// Koodin toiminta selitettyna lyhyesti
+// Kokonaislukutaulukkoharjoitus
 
 #include<iostream>
 
@@ -7,10 +7,37 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+
 int main() {
     setlocale(LC_ALL, "fi_FI");
     
+    // Kokonaislukutaulukko ja "tulos" muuttuja keskiarvoa varten
+    int taulukko[10], tulos = 0;
+
+    // Silmukka, jossa kysytaan syote ja asetetaan se taulukkoon
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Anna tallennettava tenttitulos: ";
+        cin >> taulukko[i];
+    }
+
+    // Tulosteen muotoili
+    cout << endl; 
+
+    // Tulostetaan taulukon numerot
+    for (int i = 0; i < 10; i++)
+    {   
+        cout << "Tenttitulos " << i + 1 << " on: " << taulukko[i] << endl;
+    }
+
+    // Summataan "tulos" muuttujaan kaikki taulukon numerot
+    for (int i = 0; i < 10; i++)
+    {
+        tulos += taulukko[i]; 
+    }
     
+    // Lasketaan ja tulostetaan taulukon keskiarvo float-tyyppisena
+    cout << "Tenttitulosten keskiarvo on: " << tulos / 10.0;
 
     return 0;
 }
