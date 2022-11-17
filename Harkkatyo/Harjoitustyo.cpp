bf098b33c722a857f1fcbc75a5770acb456781d5
Jokke <joakim.ijas@gmail.com>
2022-11-17 17:55:40 +0200

    todo ja huonevarausfunktion aloitus

--- Harkkatyo/Harjoitustyo.cpp
+++ Harkkatyo/Harjoitustyo.cpp
@@ -7,10 +7,45 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+/* TODO
+
+1. Huoneen varaaminen
+
+2. 
+
+
+*/
+
+
+int HuoneVaraus(int huone){
+
+
+    
+}
+
 int main() {
     setlocale(LC_ALL, "fi_FI");
     
-    int huoneiden_lkm[300] = { 0 };
+    int varattava_huone; 
+    int varatut_huoneet[300] = { 0 };
+    char valikko;
+
+    cout << "Tervetuloa!!" << endl;
+    cout << "Mitä haluaisitte tehdä?" << endl;
+
+
+    while (true)
+    {
+        
+        cout << R"(Jos haluatte varata huoneen valitkaa " V " , jos haluatte poistua valitkaa " P " : )";
+        cin >> valikko; 
+
+        if (valikko == 'V' || valikko == 'v')
+        {
+            HuoneenVaraus();
+        }
+        
+    }
     
     
     return 0;
