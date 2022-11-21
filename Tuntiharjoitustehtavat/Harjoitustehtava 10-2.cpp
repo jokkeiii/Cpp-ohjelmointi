a93199a58eb2de0169fcc91693b05dcd780d382e
jokija <joakim.ijas@tuni.fi>
2022-11-21 11:30:34 +0200

    created 10-2

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 10-2.cpp
@@ -0,0 +1,18 @@
+// 10. Tunti Viope 10 - 2
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+    setlocale(LC_ALL, "fi_FI");
+    
+    
+
+    return 0;
+}
