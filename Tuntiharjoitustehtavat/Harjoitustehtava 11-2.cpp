17eedd357ea0109ac5caad764214ba97bd45e498
Jokke <joakim.ijas@gmail.com>
2022-11-28 13:50:42 +0200

    created 11-2

---
+++ Tuntiharjoitustehtavat/Harjoitustehtava 11-2.cpp
@@ -0,0 +1,18 @@
+// 11. Tunti Viope 11 - 2
+// Koodin toiminta selitettyna lyhyesti
+
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+    setlocale(LC_ALL, "fi_FI");
+    
+    
+
+    return 0;
+}
