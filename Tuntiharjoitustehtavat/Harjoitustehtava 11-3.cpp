540706cfd53fc50604adf60a475763becc991dbe
Jokke <joakim.ijas@gmail.com>
2022-11-28 17:22:44 +0200

    put menu in while loop

--- Tuntiharjoitustehtavat/Harjoitustehtava 11-3.cpp
+++ Tuntiharjoitustehtavat/Harjoitustehtava 11-3.cpp
@@ -2,84 +2,110 @@
 // Lotto-ohjelma
 
 #include<iostream>
-#include<random>
+#include<ctime>
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::random_device;
-using std::uniform_int_distribution;
+using namespace std;
 
-void lottoKone(int &lottorivi[7], int &bonus[3]){
-    random_device rand;
-    uniform_int_distribution<int> dist(1,9);
-    cout << dist(rand) << " ";
+// Vakio lottonumeroiden maaraan
+const int NUMEROIDEN_MAARA = 10;
+
+void lottoKone(int &lottorivi[NUMEROIDEN_MAARA]){
+    
+    
 }
+
+void lottorivinTarkastus(int lottorivi[NUMEROIDEN_MAARA]){
+
+    cout << "Lottorivisi on: ";
+        
+        for (int i = 0; i < 7; i++)
+        {
+            cout << lottorivi[i] << ", ";
+            
+        }
+        cout << "ja bonusnumerot ";
+        for (int i = 7; i < NUMEROIDEN_MAARA; i++)
+        {
+            if (i == 9)
+            {
+                cout << lottorivi[i] << ", ";
+            }else
+            {
+                cout << lottorivi[i] << " ";
+            }
+            
+        }
+
+}
+
 
 int main() {
     setlocale(LC_ALL, "fi_FI");
     
-    int lottorivi[7] = {0}, bonus[3] = {0};
-    char valikko;
+    int lottorivi[NUMEROIDEN_MAARA] = {0};
+    char valikko = '';
 
     cout << "Tervetuloa helppoon lottoon, voiton saaminen on helppoa!" << endl << endl;
 
-    menu:
-
-    cout << "Mitä haluaisit tehdä? (Syötä numero 1-4)" << endl;
-    cout << "1: Katso palkintosummat " << endl;
-    cout << "2: Pelaa koneen arpomalla rivillä" << endl;
-    cout << "3: Pelaa omilla numeroilla" << endl;
-    cout << "4: Lopeta pelaaminen" << endl;
-
-    cin >> valikko;
-
-    cout << endl;
-
-    // Valikon vaihtoehdot
-    switch (valikko)
+    while (true)
     {
-    case '1':
+        cout << "Mitä haluaisit tehdä? (Syötä numero 1-4)" << endl;
+        cout << "1: Katso palkintosummat " << endl;
+        cout << "2: Pelaa koneen arpomalla rivillä" << endl;
+        cout << "3: Pelaa omilla numeroilla" << endl;
+        cout << "4: Lopeta pelaaminen" << endl;
 
-        cout << "Numeroita + lisänumeroita oikein\tVoittosumma" << endl;
-        cout << "7\t\t\t\t\t10 000 000 e" << endl;
-        cout << "6+1\t\t\t\t\t1 000 000 e" << endl;
-        cout << "6\t\t\t\t\t100 000 e" << endl;
-        cout << "5\t\t\t\t\t2000 e" << endl;
-        cout << "4\t\t\t\t\t50 e" << endl;
-        cout << "3+1\t\t\t\t\t10 e" << endl;
-        cout << "2\t\t\t\t\t2 e" << endl;
-        cout << "1\t\t\t\t\t1 e" << endl;
-        cout << "--------------------------------------------------------------------------------" << endl << endl;
-        goto menu;
+        cin >> valikko;
 
-        break;
-    case '2':
+        cout << endl;
 
-        // Kutsutaan funktiota taulukoilla(ovat viela tyhjat)
-        lottoKone(lottorivi[7], bonus[3]);
+        // Valikon vaihtoehdot
+        switch (valikko)
+        {
+        case '1':
 
-        break;
-    case '3':
-    
-        cout << "Valitettavasti valitsemasi vaihtoehto ei ole vielä käytössä. " << endl;
+            cout << "Numeroita + lisänumeroita oikein\tVoittosumma" << endl;
+            cout << "7\t\t\t\t\t10 000 000 e" << endl;
+            cout << "6+1\t\t\t\t\t1 000 000 e" << endl;
+            cout << "6\t\t\t\t\t100 000 e" << endl;
+            cout << "5\t\t\t\t\t2000 e" << endl;
+            cout << "4\t\t\t\t\t50 e" << endl;
+            cout << "3+1\t\t\t\t\t10 e" << endl;
+            cout << "2\t\t\t\t\t2 e" << endl;
+            cout << "1\t\t\t\t\t1 e" << endl;
+            cout << "--------------------------------------------------------------------------------" << endl << endl;
 
-        goto menu;
-        break;
-    case '4':
-    
-        cout << "--------------------------------------------------------------------------------" << endl << endl;
-        cout << "Tervetuloa uudestaan.";
+            break;
+        case '2':
 
-        break;
-    default:
+            // Kutsutaan funktiota taulukoilla(ovat viela tyhjat)
+            lottoKone(lottorivi[NUMEROIDEN_MAARA]);
+            
+            lottorivinTarkastus(lottorivi[NUMEROIDEN_MAARA]);
+            
+            break;
+        case '3':
+        
+            cout << "Valitettavasti valitsemasi vaihtoehto ei ole vielä käytössä. " << endl;
 
-        cout << "--------------------------------------------------------------------------------" << endl << endl;
-        cout << "\tValitettavasti valitsemasi vaihtoehto ei ole käytössä. " << endl << endl;
-        cout << "--------------------------------------------------------------------------------" << endl << endl;
+            break;
+        case '4':
+        
+            cout << "--------------------------------------------------------------------------------" << endl << endl;
+            cout << "Tervetuloa uudestaan.";
 
-        goto menu;
+            break;
+        default:
+
+            cout << "--------------------------------------------------------------------------------" << endl << endl;
+            cout << "\tValitettavasti valitsemasi vaihtoehto ei ole käytössä. " << endl << endl;
+            cout << "--------------------------------------------------------------------------------" << endl << endl;
+
+            break;
+        }
     }
+
+    
 
     return 0;
 }
