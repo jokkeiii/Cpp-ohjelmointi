09c4d47d48158901bc5868b4a6507b7a6f87b0e4
Jokke <joakim.ijas@gmail.com>
2022-12-03 17:29:24 +0200

    funktion optimoimista

--- Harkkatyo/rand_test.cpp
+++ Harkkatyo/rand_test.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 int main(){
 
-    srand(time(NULL));
+    // Randin siemennys, randin vakio yliajetaan ajasta
+    srand(time(NULL)); 
     
     int f_hinta_per_yo;
     
+    // Silmukkaa ajetaan kunnes rand antaa tulokseksi yli 80 tai alle 100
     do
     {
-        f_hinta_per_yo = rand() % 100 + 1;
+        f_hinta_per_yo = rand() % 100;
 
         cout << endl << f_hinta_per_yo << endl;
-    
-    } while (f_hinta_per_yo < 30 || f_hinta_per_yo > 70);
-    
+
+    } while (f_hinta_per_yo < 80 || f_hinta_per_yo > 100);
+
     cout << endl << "Final is " << f_hinta_per_yo << endl << endl;
     
     return 0;
