17cf599bc7464f91b4d96e1fdc86bc8ff449c8c0
jokija <joakim.ijas@tuni.fi>
2022-12-05 16:11:51 +0200

    more testing

--- Testipatkat/rand_test.cpp
+++ Testipatkat/rand_test.cpp
@@ -4,20 +4,34 @@ using namespace std;
 int main(){
 
     // Randin siemennys, randin vakio yliajetaan ajasta
-    srand(time(NULL)); 
     
-    int f_hinta_per_yo;
+    
+    int f_hinta_per_yo, n = 0;
     
     // Silmukkaa ajetaan kunnes rand antaa tulokseksi yli 80 tai alle 100
+    /*
     do
     {
-        f_hinta_per_yo = rand() % 100;
+        f_hinta_per_yo = rand() % 300;
 
         cout << endl << f_hinta_per_yo << endl;
 
     } while (f_hinta_per_yo < 80 || f_hinta_per_yo > 100);
-
-    cout << endl << "Final is " << f_hinta_per_yo << endl << endl;
+    */
+   for (int i = 0; i < 10; i++)
+   {
+        while (f_hinta_per_yo != 300)
+        {
+            srand(time(NULL));
+            
+            f_hinta_per_yo = rand() % 300 + 1;
+            n++;
+        }
+    
+        cout << f_hinta_per_yo << ", " << n << endl << endl;
+   }
+   
+    // cout << endl << "Final is " << f_hinta_per_yo << endl << endl;
     
     return 0;
 }
