2507f0922d2a250b6db6c28965f172fd5dd05e97
Jokke <joakim.ijas@gmail.com>
2022-12-08 17:49:10 +0200

    added varausnumero, modded randAle and consts

--- Harkkatyo/harjoitustyo.h
+++ Harkkatyo/harjoitustyo.h
@@ -5,17 +5,16 @@
 // Vakiot
 const int VARAUKSEN_KESTO_MIN = 0;
 const int VARAUKSEN_KESTO_MAX = 45;
-const int HUONEIDEN_MAX_LKM = 300;
-const int RAND_HUONE_MAX = 70;
-const int RAND_HUONE_MIN = 30;
-const int RAND_HINTA_MAX = 100;
-const int RAND_HINTA_MIN = 80;
+const int HUONEIDEN_MAARA_MAX = 300;
+const int HUONEIDEN_MAARA_MIN = 40;
+const int HINTA_YKSIO = 100;
+const int HINTA_KAKSIO = 80;
 
 // Tietueet
 struct HuoneVaraukset
 {
     std::string varaajan_koko_nimi = "Tyhjä";
-    int huoneen_numero = 0, varauksen_kesto = 0;
+    int huoneen_numero = 0, varauksen_kesto = 0, varaus_numero = 0;
 };
 
 
@@ -24,5 +23,6 @@ bool onkoHuoneVarattu(const HuoneVaraukset &Varaukset, int fvarattava_huone);
 HuoneVaraukset luoVaraus(HuoneVaraukset &Varaukset, int fvarattava_huone);
 void tulostaVaraus(const HuoneVaraukset &Varaukset, int hinta_per_yo);
 int randHuoneidenMaara();
-int randHuoneHinta();
+float randAlennuksenMaara();
 int randHuoneenNumero(int huoneiden_maara);
+int randVarausNumero(HuoneVaraukset &Varaukset, int huoneiden_maara);
